c0d130a0c3bf704e3da7553f240c8b0b6269b368
Mansuri Junaid <junaidmansuri989@gmail.com>
2025-02-02 14:51:01 +0530

    Deploy to gh-pages

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+import Navbar from './Components/Navbar/Navbar.jsx'
+import Home from './Components/Home/Home.jsx'
+import About from './Components/About/About.jsx'
+import Experience from './Components/Experience/Experience.jsx'
+import Projects from './Components/Projects/Projects.jsx'
+import Contact from './Components/Contact/Contact.jsx'
+import Footer from './Components/Footer/Footer.jsx'
+function App() {
+
+
+  return (
+    <>
+    <Home />
+  <Navbar />
+
+  < About/>
+  < Experience/>
+  < Projects/>
+  < Contact/>
+  < Footer />
+    </>
+  )
+}
+
+export default App
+
